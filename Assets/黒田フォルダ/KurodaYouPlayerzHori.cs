78a4051e143da8d4297251943b8d881433bdef2e
kuromaaaaa <kuromaxa@gmail.com>
2023-07-13 13:47:34 +0900

    自分のシーンにマリオもどきを置いた

---
+++ Assets/黒田フォルダ/KurodaYouPlayerzHori.cs
@@ -0,0 +1,30 @@
+
+using UnityEngine;
+
+public class KurodaYouPlayerzHori : MonoBehaviour
+{
+    [SerializeField] float _speed = 1;
+    [SerializeField] float _dashSpeed = 5f;
+    float _dash = 1;
+    Rigidbody2D _rb;
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+    public void Update()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        _rb.velocity =new Vector2(h * _speed * _dash, _rb.velocity.y);
+
+
+        if (Input.GetKey(KeyCode.Return))
+        {
+            _dash = _dashSpeed;
+        }
+        else
+        {
+            _dash = 1;
+        }
+    }
+}
