f3ef0325b16f39e30c7736bd05c38d93661189c5
kuromaaaaa <kuromaxa@gmail.com>
2023-07-15 20:28:20 +0900

    ノコノコ制作中

---
+++ Assets/黒田フォルダ/Enemy.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEditor.Experimental.GraphView.GraphView;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] GameObject _stompPrefub;
+
+    public GameObject stompPrefub { get { return _stompPrefub; } }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Attack")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Instantiate(_stompPrefub, this.transform.position, _stompPrefub.transform.rotation);
+            Destroy(this.gameObject);
+
+        }
+    }
+
+}
