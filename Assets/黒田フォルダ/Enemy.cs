c2a12eaa061f3bbfedc8f55bc852c895dd870572
kuromaaaaa <kuromaxa@gmail.com>
2023-07-18 21:10:08 +0900

    エネミー調整

--- Assets/黒田フォルダ/Enemy.cs
+++ Assets/黒田フォルダ/Enemy.cs
@@ -9,6 +9,8 @@ public class Enemy : MonoBehaviour
     [SerializeField] GameObject _stompPrefub;
     [SerializeField] float _moveSpeed;
     Rigidbody2D _rb;
+    Vector2 _moveDirection = new Vector2(-0.6f ,0);
+    LayerMask _wallLayer = 0;
 
     public GameObject stompPrefub { get { return _stompPrefub; } }
     // Start is called before the first frame update
@@ -21,10 +23,12 @@ public class Enemy : MonoBehaviour
     void Update()
     {
         Move();
-        if(GetComponent<CircleCollider2D>().enabled == false)
+        
+        if(GetComponent<CircleCollider2D>() != null && GetComponent<CircleCollider2D>().enabled == false || GetComponent<BoxCollider2D>() != null && GetComponent<BoxCollider2D>().enabled == false)
         {
             _rb.rotation += 1;
         }
+
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -51,7 +55,16 @@ public class Enemy : MonoBehaviour
 
     public virtual void Move()
     {
-        _rb.velocity = new Vector2(-1 * _moveSpeed, _rb.velocity.y);
+        Vector2 start = this.transform.position;
+        Debug.DrawLine(start, start + _moveDirection);
+        RaycastHit2D hitWall = Physics2D.Linecast(start , start + _moveDirection, _wallLayer);
+
+        if(hitWall.collider)
+        {
+            Debug.Log("Hitwall");
+            _moveDirection.x *= -1;
+        }
+        _rb.velocity = new Vector2(_moveDirection.x * _moveSpeed, _rb.velocity.y);
 
     }
 }
