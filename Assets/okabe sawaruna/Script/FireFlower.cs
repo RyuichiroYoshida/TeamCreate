e1c95b6eabec5418318406a3ab12170f711251ef
okabe783 <134473737+okabe783@users.noreply.github.com>
2023-07-19 20:30:41 +0900

    ファイアーマリオに変身

--- Assets/okabe sawaruna/Script/FireFlower.cs
+++ Assets/okabe sawaruna/Script/FireFlower.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class FireFlower : ItemBase
 {
+    [SerializeField] GameObject _fireMarioPrefab;
     public override void GetItem()
     {
+        
+        Instantiate(_fireMarioPrefab, transform.position, Quaternion.identity);
         Debug.Log("ファイアーマリオになりました");
         Destroy(this.gameObject);
+        Destroy(_mario);
     }
 }
